bbe2ba25b21942124ea74b2030e45b300f776cf5
Alain Lavoie <alavoie@cloudops.com>
2025-10-06 14:51:59 -0400

    Fixed some installation of mysql and added to git ignore inventory.ini

--- ansible/scripts/mysql_data.sh
+++ ansible/scripts/mysql_data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [ ! -d "/data/mysql" ]; then
-    sudo mkfs -t ext4 /dev/vdb
-    sudo mkdir /data/mysql
-    sudo mount /dev/vdb /data/mysql
-    #echo "/dev/vdb               /data/mysql                  ext4    defaults,nofail 0 2" | sudo tee -a /etc/fstab
+if [ ! -d "{{ mysql_mount_point }}" ]; then
+    sudo mkfs -t ext4 {{ mysql_data_disk }}
+    sudo mkdir {{ mysql_mount_point }}
+    sudo mount {{ mysql_data_disk }} {{ mysql_mount_point }}
+    #echo "{{ mysql_data_disk }}               {{ mysql_mount_point }}                  ext4    defaults,nofail 0 2" | sudo tee -a /etc/fstab
+    sudo rm -r {{ mysql_mount_point }}/*
 fi
 touch /tmp/.mounted_by_mysql_data_script
